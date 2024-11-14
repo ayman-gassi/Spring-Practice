6a272e2ddb54d3be2b6259d2bb37154a0378123e
ayman-gassi <itsmeaymane2003@gmail.com>
2024-11-14 21:25:52 +0100

    small update update

--- TP05/src/main/java/com/mundiapolis/tp05/entities/Patient.java
+++ TP05/src/main/java/com/mundiapolis/tp05/entities/Patient.java
@@ -2,7 +2,10 @@ package com.mundiapolis.tp05.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Date;
 
@@ -14,8 +17,11 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min = 5 , max = 15)
     private String nom;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private String email;
     private boolean malade;
--- TP05/src/main/java/com/mundiapolis/tp05/web/PatientController.java
+++ TP05/src/main/java/com/mundiapolis/tp05/web/PatientController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -26,7 +27,7 @@ public class PatientController {
     @GetMapping
     public String getAllPatients(Model model,
                                  @RequestParam(value = "page",defaultValue = "0") int page ,
-                                 @RequestParam(value = "size",defaultValue = "5") int size ,
+                                 @RequestParam(value = "size",defaultValue = "7") int size ,
                                  @RequestParam(value = "word",defaultValue = "") String word ) {
         Page<Patient> pagePatients =  patientService.getAllPatients(word , page,size);
         model.addAttribute("allPatients",pagePatients.getContent());
@@ -53,9 +54,50 @@ public class PatientController {
             throw new RuntimeException("Patient not found");
         }
     }
+
+    @GetMapping("/form")
+    public String formPatient(Model model){
+        model.addAttribute("patient", new Patient());
+        return "formPatient";
+
+    }
+    @PostMapping("/savePatient")
+    public String savePatient(Model model, @Valid Patient p, BindingResult
+            bindingResult){
+        if(bindingResult.hasErrors()){
+            return "formPatient";
+        }
+
+        patientService.savePatient(p);
+        model.addAttribute("patient", p);
+        return "formPatientComfirmation";
+
+    }
     @GetMapping("/delete")
     public String deletePatient(Long id , String word , int page) {
         patientService.deletePatient(id);
         return "redirect:/patients?page="+page+"&word="+word;
     }
+    @GetMapping("/edit")
+    public String edit(Model model,Long id){
+            Patient patient = patientService.getPatientById(id).orElse(null);
+            if (patient == null){
+                throw new RuntimeException("Patient Doesn't Exist");
+            }
+            else {
+                model.addAttribute("patient",patient);
+                return "formEditPatient";
+            }
+    }
+    @PostMapping("/editPatient")
+    public String editPatient(Model model, @Valid Patient p, BindingResult
+            bindingResult){
+        if(bindingResult.hasErrors()){
+            return "formEditPatient";
+        }
+        patientService.savePatient(p);
+        return "redirect:/patients";
+
+    }
 }
+
